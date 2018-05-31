062a4954cd97591f99e90f54f8618aef8c36271b
euwars <nazifi10@gmail.com>
2018-05-31 23:13:49 +0200

    Samourai API Provider, TXs, HD, Unspent

---
+++ Sentinel/API/HD.swift
@@ -0,0 +1,37 @@
+//
+//  Multiaddr.swift
+//  Sentinel
+//
+//  Created by Farzad Nazifi on 31.05.18.
+//  Copyright © 2018 Samourai. All rights reserved.
+//
+
+import Foundation
+
+extension Samourai {
+    struct HD: Codable {
+        struct Wallet: Codable {
+            let final_balance: Int
+        }
+        
+        let wallet: Wallet
+        
+        struct Info: Codable {
+            let latest_block: Samourai.Transaction.Block
+        }
+        
+        let info: Info
+        
+        struct Address: Codable {
+            let address: String
+            let final_balance: Int
+            let account_index: Int
+            let change_index: Int
+            let n_tx: Int
+        }
+        
+        let addresses: [Address]
+        
+        let txs: [Transaction]
+    }
+}
---
+++ Sentinel/API/Transaction.swift
@@ -0,0 +1,53 @@
+//
+//  Transaction.swift
+//  Sentinel
+//
+//  Created by Farzad Nazifi on 31.05.18.
+//  Copyright © 2018 Samourai. All rights reserved.
+//
+
+import Foundation
+
+extension Samourai {
+    struct Transaction: Codable {
+        let txid: String
+        let size: Int
+        let vsize: Int
+        let version: Int
+        let locktime: Int
+        
+        struct Block: Codable {
+            let height: Int
+            let hash: String
+            let time: Int
+        }
+        
+        let block: Block
+        
+        struct Input: Codable {
+            struct Outpoint: Codable {
+                let txid: String
+                let vout: Int
+                let value: Int
+                let scriptpubkey: String
+            }
+            
+            let n: Int
+            let outpoint: Outpoint
+            let sig: String
+            let seq: Int
+        }
+        
+        let inputs: [Input]
+        
+        struct Output: Codable {
+            let n: Int
+            let value: Int
+            let scriptpubkey: String
+            let type: String
+            let address: String
+        }
+        
+        let outputs: [Output]
+    }
+}
