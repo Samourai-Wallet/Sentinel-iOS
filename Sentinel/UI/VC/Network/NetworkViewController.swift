088e08c9381ceff18b84a9992564d7760f2a0077
Gigi <dergigi@pm.me>
2020-04-20 10:39:16 +0200

    Connect UI outlets; primitive view updates

--- Sentinel/UI/VC/Network/NetworkViewController.swift
+++ Sentinel/UI/VC/Network/NetworkViewController.swift
@@ -10,10 +10,33 @@ import UIKit
 
 class NetworkViewController: UIViewController {
 
+    
+    @IBOutlet weak var buttonTor: UIButton!
+    @IBOutlet weak var buttonRenew: UIButton!
+    @IBOutlet weak var labelStatus: UILabel!
+    @IBOutlet weak var viewStreetLight: UIView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         self.title = NSLocalizedString("Network", comment: "")
+        
+        torDidStop()
+    }
+    
+    private func torDidConnect() {
+        labelStatus.text = NSLocalizedString("Enabled", comment: "")
+        viewStreetLight.backgroundColor = UIColor.green
+        buttonRenew.isHidden = false
+        buttonTor.setTitle(NSLocalizedString("DISABLE", comment: ""), for: .normal)
+    }
+    
+    private func torDidStop() {
+        labelStatus.text = NSLocalizedString("Disabled", comment: "")
+        viewStreetLight.backgroundColor = UIColor.red
+        buttonRenew.isHidden = true
+        buttonTor.setTitle(NSLocalizedString("ENABLE", comment: ""), for: .normal)
     }
 
 
