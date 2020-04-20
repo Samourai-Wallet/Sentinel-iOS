e7b55954a29b35147bc80fd623afbfbceb6afcc6
Gigi <dergigi@pm.me>
2020-04-20 12:52:28 +0200

    Add extension to easily add menu buttons

--- Sentinel/UI/Flow/HomeFlow/HomeFlowViewController.swift
+++ Sentinel/UI/Flow/HomeFlow/HomeFlowViewController.swift
@@ -94,3 +94,19 @@ class HomeFlowViewController: UIViewController, NewWalletViewControllerDelegate 
         sentinel.update()
     }
 }
+
+extension UIBarButtonItem {
+
+    static func menuButton(_ target: Any?, action: Selector, imageName: String) -> UIBarButtonItem {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.addTarget(target, action: action, for: .touchUpInside)
+
+        let menuBarItem = UIBarButtonItem(customView: button)
+        menuBarItem.customView?.translatesAutoresizingMaskIntoConstraints = false
+        menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 24).isActive = true
+
+        return menuBarItem
+    }
+}
