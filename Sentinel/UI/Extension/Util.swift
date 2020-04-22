f8bed3d6c77b37046abbdd16cd86d8e98ec6b355
Gigi <dergigi@pm.me>
2020-04-22 11:04:47 +0200

    Move UIBarButtonItem extension to Util

--- Sentinel/UI/Extension/Util.swift
+++ Sentinel/UI/Extension/Util.swift
@@ -107,3 +107,18 @@ extension UIViewController {
         present(alertVC, animated: true, completion: nil)
     }
 }
+
+extension UIBarButtonItem {
+    static func menuButton(_ target: Any?, action: Selector, imageName: String) -> UIBarButtonItem {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(named: imageName), for: .normal)
+        button.addTarget(target, action: action, for: .touchUpInside)
+
+        let menuBarItem = UIBarButtonItem(customView: button)
+        menuBarItem.customView?.translatesAutoresizingMaskIntoConstraints = false
+        menuBarItem.customView?.heightAnchor.constraint(equalToConstant: 24).isActive = true
+        menuBarItem.customView?.widthAnchor.constraint(equalToConstant: 24).isActive = true
+
+        return menuBarItem
+    }
+}
