2fd6b9cccd80b55876450f143763a671b6a4dfa7
Gigi <dergigi@pm.me>
2020-04-22 09:30:49 +0200

    Add VC to show Tor initialization on startup

---
+++ Sentinel/UI/VC/Network/InitializingTorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InitializingTorViewController.swift
+//  Sentinel
+//
+//  Created by Gigi on 22.04.20.
+//  Copyright © 2020 Samourai. All rights reserved.
+//
+
+import UIKit
+
+class InitializingTorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
