9ce164bf5edc02238dfeac08df32d9c97682a0fe
Gigi <dergigi@pm.me>
2020-05-06 11:52:53 +0200

    Connect to Dojo after connecting to Tor on startup

--- Sentinel/UI/VC/Network/InitializingTorViewController.swift
+++ Sentinel/UI/VC/Network/InitializingTorViewController.swift
@@ -31,6 +31,13 @@ class InitializingTorViewController: UIViewController {
             self.dismiss(animated: true, completion: nil)
         }
     }
+    
+    func dismissView() {
+        DispatchQueue.main.async {
+            self.activityIndicator.stopAnimating()
+            self.dismiss(animated: true, completion: nil)
+        }
+    }
 
 }
 
@@ -42,9 +49,31 @@ extension InitializingTorViewController : TorManagerDelegate {
     }
     
     func torConnFinished() {
-        DispatchQueue.main.async {
-            self.activityIndicator.stopAnimating()
-            self.dismiss(animated: true, completion: nil)
+        if Sentinel.isDojoEnabled {
+            DojoManager.shared.connectToDojoWithStoredCredentials(delegate: self)
+        } else {
+            dismissView()
         }
     }
+}
+
+extension InitializingTorViewController : DojoManagerDelegate {
+    func dojoConnProgress(_ progress: Int, localizedMessage: String) {
+        DispatchQueue.main.async {
+            self.initializingLabel.text = NSLocalizedString("Connecting to Dojo", comment: "") + " \(progress)%"
+        }
+    }
+    
+    func dojoConnFinished() {
+        dismissView()
+    }
+    
+    func dojoConnFailed(message: String) {
+        DispatchQueue.main.async {
+            self.initializingLabel.text = NSLocalizedString("Failed to connect to Dojo", comment: "")
+        }
+        dismissView()
+    }
+    
+    
 }
